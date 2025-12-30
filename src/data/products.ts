2909517e4f09fdc0fbfc9bd1af02fb3637d43594
Lucas de Lima Martinez <106637076+luscasmartinez@users.noreply.github.com>
2025-12-30 20:31:18 -0300

    juste links

--- src/data/products.ts
+++ src/data/products.ts
@@ -7,8 +7,8 @@ export const products: Product[] = [
     name: 'Pastel de Carne',
     description: 'Pastel artesanal recheado com carne moída temperada',
     price: 'R$ 8,00',
-    image: 'src/img/pastelpc.png', // Desktop
-    mobileImage: 'src/img/pastelmob.png', // Mobile
+    image: 'https://i.imgur.com/AfZ99F9.png', // Desktop
+    mobileImage: 'https://i.imgur.com/56oTPtX.png', // Mobile
     category: 'pastel'
   },
   {
@@ -16,7 +16,7 @@ export const products: Product[] = [
     name: 'Bolo Caseiro',
     description: 'Pastel crocante recheado com queijo derretido',
     price: 'R$ 7,00',
-    image: 'src/img/bolopc.png',
+    image: 'https://i.imgur.com/2aTwbGm.png',
     mobileImage: 'https://i.imgur.com/4gbqOHk.jpeg',
     // Não tem mobileImage específica, usa a mesma
     category: 'bolo'
@@ -26,8 +26,8 @@ export const products: Product[] = [
     name: 'Empadas',
     description: 'Bolo caseiro de chocolate com cobertura cremosa',
     price: 'R$ 45,00',
-    image: 'src/img/empadapc.png',
-    mobileImage: 'src/img/empadamob.png',
+    image: 'https://i.imgur.com/jrGR40F.png',
+    mobileImage: 'https://i.imgur.com/sD97w4E.png',
     category: 'bolo'
   },
   
